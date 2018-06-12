02063c982b6f05df2b798aa3eb6a73fb9ce414f8
paolapereira93 <paolamartinspe@gmail.com>
2018-06-11 22:47:12 -0300

    page rank :)

--- main.py
+++ main.py
@@ -7,10 +7,13 @@ G = nx.DiGraph()
 pages = la('pages/')
 G.add_nodes_from(pages)
 
+
 pages_ranks = {}
 eg = 0.01
+alfa = 0.1
 
 parents = {x:[] for x in pages}
+children = {x:[] for x in pages}
 
 for page in pages:
     #print(page)
@@ -25,12 +28,21 @@ for page in pages:
         
         #Tem que criar duplicatas
         parents[link_page].append(page)
+        children[page].append(link_page)
     
 #nx.draw(G, with_labels=True, font_weight='bold')
 #plt.show()
 
 
-"""
-error = 1
-while(error >= eg):
-"""
+todos_abaixo_erro = False
+while(!todos_abaixo_erro):
+    for page in pages:
+        new_pr = 0
+        #pegando os que apontam para ela
+        for parent in parents[page]:
+            new_pr += (pages_ranks[parent] / len(children[parent]))
+        new_pr = (alfa / len(pages)) + new_pr * (1 - alfa)
+        if(abs(new_pr - pages_ranks[page]) <= eg):
+            todos_abaixo_erro = True
+        pages_ranks[page] = new_pr
+        
