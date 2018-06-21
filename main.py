1e0c09011668cddb329b854bc370fcecbb128248
Evandro Duarte de Arede Junior <evandro.arede_BOLS@UDDHPBRG129F5VL.pampa.compasso>
2018-06-21 12:22:54 -0300

    tudo na tranquilidade

--- main.py
+++ main.py
@@ -2,11 +2,40 @@ from bs4 import BeautifulSoup
 from os import listdir as la
 import networkx as nx
 import matplotlib.pyplot as plt
+from sys import argv
 
-#def show_grafo(Grafo, pos, nodo )
+def show_grafo(G, pos, pages_ranks, error, conv = False):
+    if(conv):
+        plt.figure('[CONVERGIU] MAX ERROR='+str(error)[:5])
+        cmap = plt.cm.summer
+        tcol = 'black'
+        op = options2
+    else:
+        cmap = plt.cm.winter
+        plt.figure('MAX ERROR='+str(error)[:5])
+        tcol = 'red'
+        op = options1
+    
+    nx.draw(G, **op, pos=pos,
+        edge_color=range(G.number_of_edges()),
+        edge_cmap=cmap)
+    for page in pages_ranks:
+        x,y = pos[page]
+        plt.text(x-int(x < 0)*0.1,y+(0.1*(2*int(y >= 0)-1)),
+			s=str(pages_ranks[page])[:5], color=tcol)
+    plt.show()
 
-options = {
-    'node_color': '#e8e9ff',
+options1 = {
+    'node_color': '#757bff',
+    'node_size': 5000,
+    'width': 3,
+    'arrowstyle': '-|>',
+    'arrowsize': 12,
+    'with_labels': True,
+}
+
+options2 = {
+    'node_color': '#ff7575',
     'node_size': 5000,
     'width': 3,
     'arrowstyle': '-|>',
@@ -17,23 +46,19 @@ options = {
 G = nx.DiGraph()
 
 pages = la('pages/')
-#pages_name = [x.replace('.html','') for x in pages]
 G.add_nodes_from(pages, type = 'machine')
-
-#nx.draw(G, **options, pos=nx.circular_layout(G))
 
 plt.show()
 
 pages_ranks = {}
 #alfa (probabilidade do teleporte) e epsilon (erro aceitável para convergência). 
-eg = 1e-2
-alfa = 0.1
+alfa = float(argv[1])
+eg = float(argv[2])
 
 parents = {x:[] for x in pages}
 children = {x:[] for x in pages}
 
 for page in pages:
-    #print(page)
     pages_ranks[page] = 1/len(pages)
     
     html_doc = '\n'.join(open('pages/' + page, 'r').readlines())
@@ -47,35 +72,23 @@ for page in pages:
         parents[link_page].append(page)
         children[page].append(link_page)
 
-#nx.draw_networkx_labels(G)
-#labels = nx.draw_networkx_labels(G, pos=pos)
 pos = nx.circular_layout(G)
-#nx.draw(G, **options, pos=pos,
-#    edge_color=range(G.number_of_edges()),
-#    edge_cmap=plt.cm.Blues)
-
-#plt.show()
-
+show_grafo(G, pos, pages_ranks, error = 0)
 
 algum_acima_erro = True #pra entrar no loop
 while(algum_acima_erro):
     algum_acima_erro = False #assumindo que todos estao abaixo do erro
-
-    nx.draw(G, **options, pos=pos,
-    edge_color=range(G.number_of_edges()),
-    edge_cmap=plt.cm.Blues)
-    
+    error = []
     for page in pages:
-        new_pr = 0
-        #pegando os que apontam para ela
+        new_pr = 0        
+		#pegando os que apontam para ela
         for parent in parents[page]:
             new_pr += (pages_ranks[parent] / len(children[parent]))
         new_pr = (alfa / len(pages)) + new_pr * (1 - alfa)
-        if(abs(new_pr - pages_ranks[page]) >= eg):
+        error.append(abs(new_pr - pages_ranks[page]))
+        if(error[-1] >= eg):
             algum_acima_erro = True #se algum ficar acima do erro, seta pra True, para continuar loop
         pages_ranks[page] = new_pr
-        x,y = pos[page]
-        plt.text(x,y,s='',bbox=dict(facecolor='red', alpha=0.5),horizontalalignment='center')
-        plt.text(x,y+(0.1*(2*int(y >= 0)-1)),s=str(pages_ranks[page])[:5])
-        #    bbox=dict(facecolor='red', alpha=0.5),horizontalalignment='center')
-    plt.show()
+    show_grafo(G, pos, pages_ranks, error = max(error))
+show_grafo(G, pos, pages_ranks, error = max(error), conv = True)
+    
